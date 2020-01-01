1d192d78a76bb742da74b74f047bd636905dcb86
Irene <ireneog_72@hotmail.es>
2020-01-01 23:39:48 +0100

    Add css contenido central

--- index.php
+++ index.php
@@ -55,29 +55,26 @@
 
       <!-- CONTENIDO CENTRAL -->
       <div id="central">
+      <h1>Productos destacados</h1>
         <div class="product">
           <img src="assets/img/camiseta.png" alt="">
           <h2>Camiseta Azul Ancha</h2>
           <p>30 euros</p>
-          <a href="#">Comprar</a>
+          <a href="#" class="button">Comprar</a>
         </div>
-      </div>
-
-      <div id="central">
+      
         <div class="product">
           <img src="assets/img/camiseta.png" alt="">
           <h2>Camiseta Azul Ancha</h2>
           <p>30 euros</p>
-          <a href="#">Comprar</a>
+          <a href="#" class="button">Comprar</a>
         </div>
-      </div>
-
-      <div id="central">
+      
         <div class="product">
           <img src="assets/img/camiseta.png" alt="">
           <h2>Camiseta Azul Ancha</h2>
           <p>30 euros</p>
-          <a href="#">Comprar</a>
+          <a href="#" class="button">Comprar</a>
         </div>
       </div>
     </div>
