2d8d479466182be9440ff2debcd463723b96d872
Irene <ireneog_72@hotmail.es>
2020-01-03 11:59:34 +0100

    Add formulario para crear productos

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -9,12 +9,30 @@
       
       }
 
-/*
       public function crear(){
         Utils::isAdmin();
-        require_once 'views/categoria/crear.php';
+        require_once 'views/producto/crear.php';
       }
-      */
+
+      public function save(){
+        Utils::isAdmin();
+    
+        // Guardar el producto en bd
+        if( isset($_POST) && isset($_POST['nombre']) ){
+    
+          //var_dump($_POST);
+          
+          $producto = new Producto();
+          $producto->setCategoria_id($_POST['categoria']);
+          $producto->setNombre($_POST['nombre']);
+          // FALTAN EL RESTO DE CAMPOS NO OBLIGATORIOS
+          $result_save = $producto->save();
+    
+        }
+    
+        header("Location:" . BASE_URL. "producto/gestion");
+    
+      }
 
       public function gestion(){
 
---
+++ views/producto/crear.php
@@ -0,0 +1,33 @@
+<h1>Crear nuevo producto</h1>
+
+<div class="form_container">
+  <form action="<?=BASE_URL?>producto/save" method="post">
+    <label for="nombre">*Nombre</label>
+    <input type="text" name="nombre" required />
+
+    <label for="descripcion">Descripción</label>
+    <textarea name="descripcion"></textarea>
+
+    <label for="precio">Precio</label>
+    <input type="text" name="precio" />
+
+    <label for="stock">Stock</label>
+    <input type="number" name="stock" />
+
+    <label for="categoria">Categoria</label>
+    <?php $categorias = Utils::showCategorias(); ?>
+    <select name="categoria" class="class-add-padding">
+      <option value="0">-- SELECCIONE CATEGORIA --</option>
+      
+      <?php while( $cat = $categorias->fetch_object()): ?>
+        <option value="<?=$cat->id?>"><?=$cat->nombre?></option>
+      <?php endwhile; ?>
+    </select> 
+
+    <label for="imagen">Imagen</label>
+    <input type="file" name="imagen" />
+    
+
+    <input type="submit" value="Guardar" class="button-prod" />
+  </form>
+</div>
