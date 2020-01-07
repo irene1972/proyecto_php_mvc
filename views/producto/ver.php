89960d467667a653deedb9dd986ab334c101bc1e
Irene <ireneog_72@hotmail.es>
2020-01-07 18:31:01 +0100

    Add css view producto

--- views/producto/ver.php
+++ views/producto/ver.php
@@ -1,15 +1,20 @@
 <?php if( isset($prod) && is_object($prod) ): ?>
   <h1><?=$prod->nombre?></h1>
 
-  <?php if( $prod->imagen != null ): ?>  
-    <img src="<?=BASE_URL?>uploads/images/<?=$prod->imagen?>" alt="">
-  <?php else: ?>
-    <img src="<?=BASE_URL?>assets/img/camiseta.png" alt="">
-  <?php endif; ?>
- 
-  <p><?=$prod->descripcion?></p>
-  <p><?=$prod->precio?></p>
-  <a href="#" class="button">Comprar</a>
+  <div id="detail-product">
+    <div class="image">
+      <?php if( $prod->imagen != null ): ?>  
+        <img src="<?=BASE_URL?>uploads/images/<?=$prod->imagen?>" alt="">
+      <?php else: ?>
+        <img src="<?=BASE_URL?>assets/img/camiseta.png" alt="">
+      <?php endif; ?>
+    </div>
+    <div class="data">
+      <p class="description"><?=$prod->descripcion?></p>
+      <p class="price"><?=$prod->precio?> €</p>
+      <a href="#" class="button">Comprar</a>
+    </div>
+  </div>
 
 <?php else: ?>
   <h1>El producto no existe</h1>
