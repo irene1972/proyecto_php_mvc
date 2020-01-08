1d78a637d7486ade5b978a1416502e40a4a4f0da
Irene <ireneog_72@hotmail.es>
2020-01-08 12:23:49 +0100

    Fix Pedido confirmado

--- views/pedido/confirmado.php
+++ views/pedido/confirmado.php
@@ -1,6 +1,6 @@
-<?php if( isset($_SESSION['pedido']) && $_SESSION['pedido'] == 'confirmed' ): ?>
+<?php if( isset($_SESSION['pedido']) && $_SESSION['pedido'] == 'completed' ): ?>
   <h1>Tu peido se ha confirmado</h1>
   <p>Tu pedido ha sido guardado con exito, una vez que realizes la transferencia bancaria con el coste del pedido será procesado y enviado.</p>
-<?php elseif( isset($_SESSION['pedido']) && $_SESSION['pedido'] != 'confirmed' ): ?>
+<?php elseif( isset($_SESSION['pedido']) && $_SESSION['pedido'] != 'completed' ): ?>
   <h1>Tu pedido NO ha sido procesado. Para más detalles contacta con 627023398</h1>
 <?php endif; ?>
